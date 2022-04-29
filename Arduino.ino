a83afed296f8e92788f62891af80c53f869d7458
kim geon han <62462552+gokimkq123@users.noreply.github.com>
2022-04-29 16:24:14 +0900

    Update Arduino.ino

--- Arduino.ino
+++ Arduino.ino
@@ -1,17 +1,21 @@
-const int moterPin = 9;
+#include<Servo.h>
+Servo servo;
+
+const int moterPin = 9,ServoPin = 2,lm35Pin = A0,TRIG = 13,ECHO = 12;
 float temperature;  
-int reading;  
-int lm35Pin = A0;
-
-
+int reading,value = 0;
+long duration,cm;
 
 void setup() {
   Serial.begin(9600);
   pinMode(moterPin,OUTPUT);
+  pinMode(TRIG,OUTPUT);
+  pinMode(ECHO,INPUT);
   analogReference(INTERNAL);
+  servo.attach(2);
 }
 
-void moter(){
+void dcmoter(){
   analogWrite(moterPin,500);
 }
 void tmper(){
@@ -22,7 +26,31 @@ void tmper(){
     delay(1000);
 }
 
+void sv(){
+    for(int i=0;i<180;i+=45){
+      value = i;
+      servo.write(value), delay(500);
+    }
+}
+
+void chomfa(){
+   digitalWrite (TRIG, HIGH);  // 트리거 핀 HIGH 펄스
+   delayMicroseconds (10);        // 10 μs 지연
+   digitalWrite (TRIG, LOW);   // 8 Cycle 초음파 출력
+
+   duration = pulseIn (ECHO, HIGH); // HIGH가 유지된 시간 반환
+   cm = duration / 58.0; // 거리 = 왕복에 걸린 시간 ÷ 58
+
+   Serial.print ("distance: ");
+   Serial.print (cm);
+   Serial.println (" cm");
+   delay (1000);
+}
+
 void loop() {
-   
+
+
+
+
    
 }
